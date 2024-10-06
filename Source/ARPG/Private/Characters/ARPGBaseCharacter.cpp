7a3da17d01ddf0b12090d8e4a264abb51e80b312
Patryk Basa <patryk9595@gmail.com>
2024-10-06 12:27:13 +0200

    -Dummy classes Hero Characte, Hero Controller -Simplified Base Character class

--- Source/ARPG/Private/Characters/ARPGBaseCharacter.cpp
+++ Source/ARPG/Private/Characters/ARPGBaseCharacter.cpp
@@ -3,32 +3,16 @@
 
 #include "Characters/ARPGBaseCharacter.h"
 
+#include "Components/SkeletalMeshComponent.h"
+
 // Sets default values
 AARPGBaseCharacter::AARPGBaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
 
-}
-
-// Called when the game starts or when spawned
-void AARPGBaseCharacter::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AARPGBaseCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AARPGBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	GetMesh()->bReceivesDecals = false;
 
 }
 
