b0683e195f3b64e4e9b662d9c611dd1ae54b977d
Patryk Basa <patryk9595@gmail.com>
2024-10-06 13:15:34 +0200

    Add simple movement tags from c++

---
+++ Source/ARPG/Public/ARPGGameplayTags.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+/**
+ * 
+ */
+namespace ARPGGameplayTags
+{
+	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
