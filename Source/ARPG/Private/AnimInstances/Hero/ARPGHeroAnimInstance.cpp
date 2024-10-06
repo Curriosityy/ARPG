15c7dddd15af66beddf3f53c0a50bebba7ce514e
Patryk Basa <patryk9595@gmail.com>
2024-10-06 15:39:23 +0200

    Hero relaxed state

--- Source/ARPG/Private/AnimInstances/Hero/ARPGHeroAnimInstance.cpp
+++ Source/ARPG/Private/AnimInstances/Hero/ARPGHeroAnimInstance.cpp
@@ -3,3 +3,27 @@
 
 #include "AnimInstances/Hero/ARPGHeroAnimInstance.h"
 
+#include "Characters/ARPGHeroCharacter.h"
+
+void UARPGHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if(OwningCharacter)
+		OwningHeroCharacter = Cast<AARPGHeroCharacter>(OwningCharacter);
+}
+
+void UARPGHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if(bHasAcceleration)
+	{
+		ElapsedIdleTime = 0;
+		bShouldEnderRelaxState = false;
+	}else
+	{
+		ElapsedIdleTime+=DeltaSeconds;
+		bShouldEnderRelaxState = ElapsedIdleTime >= EnterRelaxedStateThreshold;
+	}
+}
--- Source/ARPG/Public/AnimInstances/Hero/ARPGHeroAnimInstance.h
+++ Source/ARPG/Public/AnimInstances/Hero/ARPGHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/ARPGCharacterAnimInstance.h"
 #include "ARPGHeroAnimInstance.generated.h"
 
+class AARPGHeroCharacter;
 /**
  * 
  */
@@ -13,5 +14,22 @@ UCLASS()
 class ARPG_API UARPGHeroAnimInstance : public UARPGCharacterAnimInstance
 {
 	GENERATED_BODY()
+
+
+protected:
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<AARPGHeroCharacter> OwningHeroCharacter = {};
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly,Category="AnimData|LocomotionData")
+	bool bShouldEnderRelaxState = {};
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,Category="AnimData|LocomotionData")
+	float EnterRelaxedStateThreshold = {5.f};
+
+	float ElapsedIdleTime = {};
 	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
 };
