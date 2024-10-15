81732daea87d3369363c35f7d5baa9a4d9b88586
DESKTOP-6MAF2IN\Patryk <patryk9595@gmail.com>
2024-10-15 15:51:47 +0200

    Setup sense for AI

--- Source/ARPG/Private/Controllers/ARPGAIController.cpp
+++ Source/ARPG/Private/Controllers/ARPGAIController.cpp
@@ -4,8 +4,26 @@
 #include "Controllers/ARPGAIController.h"
 
 #include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+void AARPGAIController::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+}
 
 AARPGAIController::AARPGAIController(const FObjectInitializer& ObjectInitializer): Super(
 	ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
 {
+	SenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("SenseConfig_Sight");
+	SenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	SenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	SenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	SenseConfig_Sight->SightRadius = 5000.f;
+	SenseConfig_Sight->LoseSightRadius = 0.f;
+	SenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+
+	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("PerceptionComponent");
+	PerceptionComponent->ConfigureSense(*SenseConfig_Sight);
+	PerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	PerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnPerceptionUpdated);
 }
--- Source/ARPG/Public/Controllers/ARPGAIController.h
+++ Source/ARPG/Public/Controllers/ARPGAIController.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "ARPGAIController.generated.h"
 
+class UAISenseConfig_Sight;
 /**
  * 
  */
@@ -14,6 +16,15 @@ class ARPG_API AARPGAIController : public AAIController
 {
 	GENERATED_BODY()
 
+	// UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="AI", meta=(AllowPrivateAccess))
+	// TObjectPtr<UAIPerceptionComponent> PerceptionComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="AI", meta=(AllowPrivateAccess))
+	TObjectPtr<UAISenseConfig_Sight> SenseConfig_Sight;
+
 public:
 	AARPGAIController(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION()
+	virtual void OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
 };
